cd42dfc42eecd18b31c0c4de4189c2dcded9a603
Kim Dogyeong <pcs6389@gmail.com>
2025-04-22 18:12:36 +0900

    Relation_Mart 예제

---
+++ src/main/java/com/example/mart/repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package com.example.mart.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.mart.entity.Item;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
---
+++ src/main/java/com/example/mart/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.example.mart.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.mart.entity.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
